ad6b937bf2f7ba0dac6f3d8ce9ad949de9e068c9
Andrew Jaffe <adjaffeb@gmail.com>
2024-02-26 16:09:03 -0500

    Add files via upload

---
+++ Constants.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+struct Constants {
+    static let pi: Double = 3.141592653589793
+    static let e: Double = 2.718281828459045
+    static let goldenRatio: Double = 1.618033988749895
+    static let planckConstant: Double = 6.62607015e-34
+    static let speedOfLight: Double = 299792458
+    static let gravitationalConstant: Double = 6.67430e-11
+    static let avogadroNumber: Double = 6.02214076e23
+    static let gasConstant: Double = 8.314462618
+    static let boltzmannConstant: Double = 1.380649e-23
+    static let electronMass: Double = 9.10938356e-31
+    static let protonMass: Double = 1.6726219e-27
+    static let neutronMass: Double = 1.674927471e-27
+    static let fineStructureConstant: Double = 1 / 137.035999139
+    static let magneticConstant: Double = 4 * pi * 1e-7
+    static let electricConstant: Double = 1 / (4 * pi * 8.854187817e-12)
+    static let stefanBoltzmannConstant: Double = 5.670374419e-8
+    static let rydbergConstant: Double = 10973731.568160
+    static let faradayConstant: Double = 96485.33212
+    static let electronVolt: Double = 1.602176634e-19
+    static let permeabilityOfVacuum: Double = magneticConstant
+    static let permittivityOfVacuum: Double = electricConstant
+    static let coulombConstant: Double = 1 / (4 * pi * electricConstant)
+    static let planckLength: Double = 1.616255e-35
+    static let planckTime: Double = 5.391247e-44
+    static let astronomicalUnit: Double = 149597870.7
+    static let earthGravitationalAcceleration: Double = 9.80665
+}
